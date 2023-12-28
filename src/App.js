7105fbdc918e19b4e8e8e68677fd0104eae6f858
Ankur Srivastava <ankursrivastava2070@gmail.com>
2023-12-28 14:10:43 +0530

    necklace 1 is removed and spelling is updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Necklace_AR from './Pages/AR/Necklace/necklace_sample'
 import Earing_AR from './Pages/AR/Earings/earing_sample'
 import Earing2_3D from './Pages/3D/Earings/Earing2';
 import Earing13D from './Pages/3D/Earings/Earing1';
-import Necklace13D from './Pages/3D/Necklace/Nacklace1'
 import Necklace23D from './Pages/3D/Necklace/Neacklace2'
 import Necklace33D from './Pages/3D/Necklace/Necklace3'
 import Bracelet from './Pages/3D/Bracelets/Bracelet1'
@@ -30,13 +29,11 @@ function App() {
         <Route path='/earing1/1' element={<Earing13D/>}/>
         <Route path='/bracelet1/1' element={<Bracelet/>}/>
         <Route path='/earing2/2' element={<Earing2_3D/>}/>
-        <Route path='/necklace1/1' element={<Necklace13D/>}/>
         <Route path='/necklace2/2' element={<Necklace23D/>}/>
         <Route path='/necklace3/3' element={<Necklace33D/>}/>
         <Route path='/earing1/ar' element={<Earing_AR Model='Earing1' ThreeD='/earing1/1'/>}/>
         <Route path='/earing2/ar' element={<Earing_AR Model='Earing2' ThreeD='/earing2/2'/>}/>
        
-        <Route path='/necklace1/ar' element={<Necklace_AR Model='Necklace1' ThreeD='/necklace1/1'/>}/>
         <Route path='/necklace2/ar' element={<Necklace_AR Model='Necklace2' ThreeD='/necklace2/2'/>}/>
         <Route path='/necklace3/ar' element={<Necklace_AR Model='Necklace3' ThreeD='/necklace3/3'/>}/>
       </Routes>
--- src/data.js
+++ src/data.js
@@ -2,9 +2,9 @@
 export const Earings = [
     {
       id: 1,
-      name:'Earing 1',
+      name:'Earring 1',
       desc:'Glossy Gold With Silver holder',
-      price:'$ 12',
+      price:'$12',
       image: '/images/earing1.png',  
       link3D: '/earing1/1',
       linkAR: '/earing1/ar',
@@ -13,9 +13,9 @@ export const Earings = [
     },
     {
       id: 2,
-      name:'Earing 2',
-      desc:'Royal Blue Earing',
-      price:'$ 16',
+      name:'Earring 2',
+      desc:'Royal Blue Earring',
+      price:'$16',
       image: '/images/earing2.png',  
       link3D: '/earing2/2',
       linkAR: '/earing2/ar',
@@ -25,29 +25,21 @@ export const Earings = [
   ];
   
   export const Necklaces = [
+    
     {
       id: 1,
-      desc:'Triangle shaped pendant',
-      name:'Necklace 1',
-      price:'$ 10',
-      image: '/images/necklace1.png',  
-      link3D: '/necklace1/1',
-      linkAR: '/necklace1/ar'
-    },
-    {
-      id: 2,
       desc:'Rounded Gold Necklace',
-      name:'Necklace 2',
-      price:'$ 12',
+      name:'Necklace 1',
+      price:'$12',
       image: '/images/necklace2.png',  
       link3D: '/necklace2/2',
       linkAR: '/necklace2/ar'
     },
     {
-      id: 3,
+      id: 2,
       desc:'Glossy Shine With Gold Chains',
-      name:'Necklace 3',
-      price:'$ 25',
+      name:'Necklace 2',
+      price:'$25',
       image: '/images/necklace3.png',  
       link3D: '/necklace3/3',
       linkAR: '/necklace3/ar'
@@ -59,7 +51,7 @@ export const Earings = [
       id: 1,
       desc:'Shiny Gold Bracelet',
       name:'Bracelet 1',
-      price:'$ 1',
+      price:'$1',
       image: '/images/bracelet1.png',  
       link3D: '/bracelet1/1',
     },
@@ -71,7 +63,7 @@ export const Earings = [
       id: 1,
       desc:'Silver Ring',
       name:'Ring 1',
-      price:'$ 1',
+      price:'$1',
       image: '/images/ring1.png',  
       link3D: '/ring1/1',
     },
